dbeb6b20b6d6d9f5362de4c0b58c3c80fbc34071
Trinh Minh Quan <121121741+q113pt@users.noreply.github.com>
2025-04-30 12:18:49 +0700

    Update index.ts
    Update user index file

--- mobile/src/models/index.ts
+++ mobile/src/models/index.ts
@@ -2,5 +2,5 @@ import Course from "./Courses"
 import Lesson from "./Lesson";
 import Section from "./Section";
 import MyCourse from "./MyCourses"
-
-export { Course,MyCourse, Lesson, Section};
+import User from "./User";
+export { Course,MyCourse, Lesson, Section, User};
